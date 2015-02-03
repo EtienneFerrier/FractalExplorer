a1ec1e16f7d69397319ced7cf1da1380f3c73cd8
Gurvan <gurvan.lhostis@gmail.com>
2015-02-03 12:06:51 +0100

    BigFloat arithmetics 2

--- BigFloat.hpp
+++ BigFloat.hpp
@@ -106,29 +106,17 @@ public:
 		a.base = -a.base;
 	}
 
-	/*  Multiplication de a par un chiffre de b, résultat ajouté à temp,
-	 *  temp doit être initialisé au préalable.
-	 *  On évite les integer overflow en utilisant la technique suivante :
-	 *	a = ah.2^16 + al ; b = bh.2^16 + bl
-	 *  mid = (ahbl + albh) mod 2^32
-	 *  carry = ((ahbl + albh) - mid).2^(-32)
-	 *	ab = (ahbh).2^32 + mid.2^16 + (albl)
-	 *     = (ahbh + carry.2^16 + midh).2^32 + (midl.2^16 + albl)
-	 *
-	 */
-	static void multDigit(BigFloat& a, BigFloat& b, int i, BigFloat& temp) {
-		bool carry = 0;
+
+	// Fonction servant à multiplier deux chiffres
+	static inline bool multDigDig(uint32_t& a, uint32_t& b, uint32_t& first, uint32_t& second, bool carry) {
 		uint32_t mask = 0xFFFF;
-		uint32_t ah, al, bh, bl, midh, midl, ahbh, ahbl, albh, albl;
+		uint32_t ah, al, bh, bl, midh, midl, ahbh, ahbl, albh, albl, temp;
 
-		// Il faut traiter le chiffre de plus basse importance séparément
-
-		uint32_t fakeDigit = 0;
 		// On se ramene à des int de 16 bits
-		ah = a[i] >> 16;
-		bh = b[i] >> 16;
-		al = mask & a[i];
-		bl = mask & b[i];
+		ah = a >> 16;
+		bh = b >> 16;
+		al = mask & a;
+		bl = mask & b;
 
 		// Ces produits ne peuvent pas faire d'overflow
 		// TODO: certaines mises en mémoire sont inutiles
@@ -141,55 +129,92 @@ public:
 
 		// On coupe le middle
 		midl = ahbl + albh;
-		temp[BIG_FLOAT_SIZE-1] += ((ahbl < midl) << 16);
+		temp = ((ahbl < midl) << 16);
+		second += ((ahbl < midl) << 16) + carry; // Ajout de la retenue liée à (ahbl + albh) et du carry issu de la multiplication précédente
+		carry = (second < temp + carry); // Initialisation de la prochaine carry
 		midh = midl >> 16;
 		midl = midl << 16;
 
-		fakeDigit += albl;
-		temp[BIG_FLOAT_SIZE] += (albl > fakeDigit); // On s’occupe ici de la retenue du chiffre 0 qui concerne donc le chiffre -1
-		carry = ((temp[BIG_FLOAT_SIZE-1]) == 0); // Le carry concerne le chiffre d’indice -2 et non pas -1 comme dans l’addition
-		fakeDigit += midl + carry;
-		carry |= (midl + 1 == 0 && carry) || (midl + carry > fakeDigit);
+		first += albl;
+		second += (albl > (first)); // On s’occupe ici de la retenue du chiffre 0 qui concerne donc le chiffre -1
+		carry |= ((second) == 0); // Le carry concerne le chiffre d’indice -2 et non pas -1 comme dans l’addition
 
-		for (int j = BIG_FLOAT_SIZE-i-2; j >= 0; j++) {
+		first += midl;
+		second += (midl > (first));
+		carry |= ((second) == 0);
 
-			// On se ramene à des int de 16 bits
-			ah = a[i] >> 16;
-			bh = b[i] >> 16;
-			al = mask & a[i];
-			bl = mask & b[i];
+		second += midh;
+		carry |= (midh > second);
 
-			// Ces produits ne peuvent pas faire d'overflow
-			// TODO: certaines mises en mémoire sont inutiles
-			ahbh = ah * bh;
-			ahbl = ah * bl;
-			albh = al * bh;
-			albl = al * bl;
+		second += ahbh;
+		carry |= (ahbh > second);
 
-			// Le carry est ce qu'on ajoute au chiffre supérieur à la fin
-
-			// On coupe le middle
-			midl = ahbl + albh;
-			temp[i+j-2] += ((ahbl < midl) << 16);
-			midh = midl >> 16;
-			midl = midl << 16;
-
-			temp[i + j - 1] += albl;
-			temp[i + j - 2] += (albl > temp[i + j - 1]); // On s’occupe ici de la retenue du chiffre 0 qui concerne donc le chiffre -1
-			carry = ((temp[i + j - 2]) == 0); // Le carry concerne le chiffre d’indice -2 et non pas -1 comme dans l’addition
-			temp[i + j - 1] += midl + carry;
-			carry |= (midl + 1 == 0 && carry) || (midl + carry > temp[i + j - 1]);
-		}
-
-
+		return carry;
 	}
-	
-	static void mult(BigFloat& a, BigFloat& b, BigFloat& res) {
-		BigFloat temp;
-		BigFloat digit;
-		for (int i = 0; i < BIG_FLOAT_SIZE; i++) {
-			digit = BigFloat(i)
+
+
+	// Fonction servant à multiplier un chiffre avec la partie entière
+	static inline int32_t multDigBase(uint32_t& a, BigFloat& b, uint32_t& first, uint32_t& second, bool carry) {
+		uint32_t fakeFirst = 0;
+		uint32_t fakeSecond = 0;
+		uint32_t ubase;
+		if (b.base >= 0) {
+			ubase = (b.base);
+			carry = multDigDig(a, ubase, first, second, carry);
+			return carry;
 		}
+		else { // Cas très chiant, il faut considérer une retenue négative
+			ubase = (-b.base);
+			carry = multDigDig(a, ubase, fakeFirst, fakeSecond, carry);
+			second -= fakeSecond;
+			first -= (second > fakeSecond);
+			bool negCarry = (first == 0xFFFFFFFF);
+
+			first -= fakeFirst;
+			negCarry |= (first > fakeFirst);
+			return -negCarry;
+		}
+	}
+
+	/*  Multiplication de a par un chiffre de b, résultat ajouté à temp,
+	 *  temp doit être initialisé au préalable.
+	 *  On évite les integer overflow en utilisant la technique suivante :
+	 *	a = ah.2^16 + al ; b = bh.2^16 + bl
+	 *  mid = (ahbl + albh) mod 2^32
+	 *  carry = ((ahbl + albh) - mid).2^(-32)
+	 *	ab = (ahbh).2^32 + mid.2^16 + (albl)
+	 *     = (ahbh + carry.2^16 + midh).2^32 + (midl.2^16 + albl)
+	 *
+	 */
+	static void multDigit(BigFloat& a, BigFloat& b, int i, BigFloat& temp) {
+		bool carry;
+
+		// Il faut traiter le chiffre de plus basse importance séparément
+		uint32_t fakeDigit = 0;
+		carry = multDigDig(a[i], b[BIG_FLOAT_SIZE - 1], fakeDigit, temp[BIG_FLOAT_SIZE - 1], 0);
+
+		for (int j = BIG_FLOAT_SIZE - i - 2; j >= 0; j++)
+			carry = multDigDig(a[i], b[j], temp[i + j + 1], temp[i + j], carry);
+
+		int32_t baseCarry;
+		if (i >= 1) {
+			baseCarry = multDigBase(a[i], b, temp[i - 1], temp[i], carry);
+			if (i == 1) {
+				temp.base += baseCarry;
+			}
+			// AJOUTER PROPAGATION CARRY
+		}
+		else {
+			uint32_t fakeBase = 0;
+			baseCarry = multDigBase(a[i], b, fakeBase, temp[0], carry);
+			// ATTENTION A GERER LE SIGNE DE FAKEBASE
+		}
+	}
+
+
+	// Multiplication de deux BigFloat. Multiplie b par chaque chiffre de a.
+	static void mult(BigFloat& a, BigFloat& b, BigFloat& res) {
+
 	}
 
 };
