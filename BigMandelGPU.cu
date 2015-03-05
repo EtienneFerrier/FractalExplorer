f8d2a2f7cec0cf2e81fe24d4d1b986e3d890e268
Etienne Ferrier <etienne.ferrier@hotmail.fr>
2015-03-05 10:47:42 +0100

    Simplification et petite optmisation

--- BigMandelGPU.cu
+++ BigMandelGPU.cu
@@ -1,10 +1,6 @@
 /*
-Cette classe implémente le calcul de l'ensemble de Mandelbrot sur GPU avec precision arbitraire
+Ce fichier implémente le calcul de l'ensemble de Mandelbrot sur GPU avec precision arbitraire
 */
-
-// Optimisations possibles :
-//// Ne pas calculer k (ou i ou j) à chaque fonction
-//// Limiter les lectures a la mem globale
 
 #pragma once
 
@@ -29,7 +25,7 @@ Cette classe implémente le calcul de l'ensemble de Mandelbrot sur GPU avec preci
         return retcode; \
 				    }
 
-// Affiche en float le BigFloat en utilisant les 2 premieres decimales uniquement
+// Affiche un grand flottant en écriture x.10^n en utilisant les 2 premieres decimales uniquement.
 void display2dec(bool pos, uint32_t* dec)
 {
 	double logx;
@@ -50,7 +46,7 @@ void display2dec(bool pos, uint32_t* dec)
 	cout << (pos ? "+" : "-") << dec[0] << " + " << decPart10 << ".10^" << (int)n10 << endl;
 }
 
-// Affiche un tableau de BigFloat de maniere brute
+// Affiche un grand flottant de maniere brute.
 void displayBigArray(bool* pos, uint32_t* dec, int arraySize)
 {
 	for (int i = 0; i < arraySize; i++)
@@ -59,7 +55,7 @@ void displayBigArray(bool* pos, uint32_t* dec, int arraySize)
 	}
 }
 
-// Affiche en float un tableau de BigFLoat en utilisant les 2 premieres decimales uniquement
+// Meme affiche que display2dec mais sur un tableau de grands flottants.
 void displayBigArray2dec(bool* pos, uint32_t* dec, int arraySize)
 {
 	for (int i = 0; i < arraySize; i++)
@@ -68,13 +64,13 @@ void displayBigArray2dec(bool* pos, uint32_t* dec, int arraySize)
 	}
 }
 
-// Convertit RGB en Uint32
+// Convertit une couleur RGB en Uint32
 __device__ uint32_t couleur(int r, int g, int b)
 {
 	return (((r << 8) + g) << 8) + b;
 }
 
-// Methode de coloration en couleur 32bits version sombre
+// Methode de coloration de fractale
 // Augmenter divFactor permet d'avoir une fréquence plus importante de variation des couleurs (mettre 1 par defaut)
 __device__ uint32_t computeColor_32_DARK(int countMax, int count, int divFactor)
 {
@@ -91,138 +87,20 @@ __device__ uint32_t computeColor_32_DARK(int countMax, int count, int divFactor)
 	else return couleur(255, 255 * m / p, 0);
 }
 
-// Initialise 1/100 en BigFLoat
-// Pourrait etre une macro preproc
-// A changer des que l'on change la taille de la fenetre
-__device__ void getStep100(bool* pos, uint32_t* dec)
-{
-	*pos = true;
-	dec[0] = 0;
-#if BIG_FLOAT_SIZE > 1
-	dec[1] = 42949673;
-#endif
-#if BIG_FLOAT_SIZE > 2
-	dec[2] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 3
-	dec[3] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 4
-	dec[4] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 5
-	dec[5] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 6
-	dec[6] = 0;
-#endif
-
-	__syncthreads();
-}
-
-
+// Charge 1/WIDTH dans le grand flottant
 __device__ void getStep(bool* pos, uint32_t* dec) {
+	
+	const unsigned int k = blockDim.z * blockIdx.z + threadIdx.z;
+
+	if (k == 1)
+	{
 		*pos = true;
-		dec[0] = 0;
-#if BIG_FLOAT_SIZE > 1
-		dec[1] = 0xffffffff/WIDTH;
-#endif
-#if BIG_FLOAT_SIZE > 2
-		dec[2] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 3
-		dec[3] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 4
-		dec[4] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 5
-		dec[5] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 6
-		dec[6] = 0;
-#endif
+		dec[1] = 0xffffffff / WIDTH;
+	}
+	else
+		dec[k] = 0;
 
 	__syncthreads();
-}
-
-// Initialise 1/256 en BigFLoat
-__device__ void getStep256(bool* pos, uint32_t* dec)
-{
-	*pos = true;
-	dec[0] = 0;
-#if BIG_FLOAT_SIZE > 1
-	dec[1] = 16766216;
-#endif
-#if BIG_FLOAT_SIZE > 2
-	dec[2] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 3
-	dec[3] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 4
-	dec[4] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 5
-	dec[5] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 6
-	dec[6] = 0;
-#endif
-
-	__syncthreads();
-}
-
-// Initialise 1/512 en BigFLoat
-__device__ void getStep512(bool* pos, uint32_t* dec)
-{
-	*pos = true;
-	dec[0] = 0;
-#if BIG_FLOAT_SIZE > 1
-	dec[1] = 16766216/2;
-#endif
-#if BIG_FLOAT_SIZE > 2
-	dec[2] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 3
-	dec[3] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 4
-	dec[4] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 5
-	dec[5] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 6
-	dec[6] = 0;
-#endif
-
-	__syncthreads();
-}
-
-// Initialise 1/600 en BigFLoat
-__device__ void getStep600(bool* pos, uint32_t* dec)
-{
-	*pos = true;
-	dec[0] = 0;
-#if BIG_FLOAT_SIZE > 1
-	dec[1] = 7158279;
-#endif
-#if BIG_FLOAT_SIZE > 2
-	dec[2] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 3
-	dec[3] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 4
-	dec[4] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 5
-	dec[5] = 0;
-#endif
-#if BIG_FLOAT_SIZE > 6
-	dec[6] = 0;
-#endif
 }
 
 // Enleve 0.5 a A 
@@ -295,7 +173,7 @@ __device__ void negate(bool* pos)
 	__syncthreads();
 }
 
-// A = A + B (In Place)
+// A <- A + B (In Place)
 // Remarque : A = A + A fonctionne
 __device__ void addIP(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 {
@@ -346,11 +224,11 @@ __device__ void addIP(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 }
 
 //Fonction servant à multiplier deux chiffres
-/*	a (reg) et b (reg) sont les copies des decimaux a (shared) et b (shared)
-tmpLittle (reg) est un tas qui contient la somme partielle a ajouter plus tard a little (shared)
-tmpBig (reg) est un tas qui contient la somme partielle a ajouter plus tard a big (shared)
-carry (reg) est un tas qui contient la somme des retenues a ajouter plus tard au decimal avant big (shared)
-Chaque appel demande une lecture SHARED -> REG de 2 cases au préalable. Possibilité d'optimiser.*/
+/*
+tmpLittle est un tas qui contient la somme partielle a ajouter plus tard a little
+tmpBig est un tas qui contient la somme partielle a ajouter plus tard a big
+carry est un tas qui contient la somme des retenues a ajouter plus tard au decimal avant big
+*/
 __device__ void multDigDig(uint32_t a, uint32_t b, uint8_t* carry, uint32_t* tmpBig, uint32_t* tmpLittle) {
 
 	uint32_t mask = 0xFFFF;
@@ -386,7 +264,7 @@ __device__ void multDigDig(uint32_t a, uint32_t b, uint8_t* carry, uint32_t* tmp
 	*carry += (*tmpBig < ahbh);
 }
 
-// A = A * B (In Place)
+// A <- A * B (In Place)
 __device__ void multIP(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 {
 	const unsigned int k = blockIdx.z*blockDim.z + threadIdx.z;
@@ -425,7 +303,7 @@ __device__ void multIP(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 	__syncthreads();
 }
 
-// A = (int)factor * A
+// A <- (int)factor * A
 __device__ void multIntIP(bool* posA, uint32_t* decA, bool posFactor, uint32_t factor)
 {
 	const unsigned int k = blockIdx.z*blockDim.z + threadIdx.z;
@@ -462,7 +340,7 @@ __device__ void multIntIP(bool* posA, uint32_t* decA, bool posFactor, uint32_t f
 	__syncthreads();
 }
 
-// A = B
+// A <- B
 __device__ void copyBig(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 {
 	const unsigned int k = blockIdx.z*blockDim.z + threadIdx.z;
@@ -476,8 +354,8 @@ __device__ void copyBig(bool* posA, uint32_t* decA, bool posB, uint32_t* decB)
 	__syncthreads();
 }
 
-// X = X*X - Y*Y
-// Y = 2*X*Y
+// X <- X*X - Y*Y
+// Y <- 2*X*Y
 __device__ void complexSquare(bool* posX, uint32_t* decX, bool* posY, uint32_t* decY, bool* posTmp, uint32_t* decTmp)
 {
 	/* ANCIENNE METHODE (2 valeurs temporaires) */
@@ -485,13 +363,11 @@ __device__ void complexSquare(bool* posX, uint32_t* decX, bool* posY, uint32_t* 
 	//copyBig(posTmp, decTmp, *posX, decX);		// tmp = X
 	//multIP(posTmp, decTmp, *posY, decY);		// tmp *= Y
 	//multIntIP(posTmp, decTmp, true, 2);		// tmp *= 2 	//// tmp = 2*X*Y
-	
 	//multIP(posX, decX, *posX, decX);			// X *= X
 	//copyBig(posSq, decSq, *posY, decY);		// Sq = Y
 	//multIP(posSq, decSq, *posY, decY);		// Sq *= Y 		//// Sq = - Y * Y
 	//negate(posSq);							// neg(Sq)
 	//addIP(posX, decX, *posSq, decSq);			// X += Sq		//// X = X*X - Y*Y
-
 	//copyBig(posY, decY, *posTmp, decTmp);		// Y = Tmp		//// Y = 2*X*Y
 
 	/* NOUVELLE METHODE (1 seule valeur temporaire) */			// Suivi des resultats intermediaires
@@ -521,9 +397,9 @@ __device__ bool testSquare(bool* posX, uint32_t* decX, bool* posY, uint32_t* dec
 	
 	bool res;
 
-	multIP(posX, decX, *posX, decX);	// X *= X
-	multIP(posY, decY, *posY, decY);	// Y *= Y
-	addIP(posX, decX, *posY, decY);		// X^2 += Y^2
+	multIP(posX, decX, *posX, decX);	// X = x^2
+	multIP(posY, decY, *posY, decY);	// Y = y^2
+	addIP(posX, decX, *posY, decY);		// X = x^2 + y^2
 
 	res = (decX[0] < 4);
 
@@ -541,7 +417,19 @@ __device__ bool testSquare(bool* posX, uint32_t* decX, bool* posY, uint32_t* dec
 	return res;
 }
 
-// Boucle d'iteration principale
+// Charge le point de depart des iterations dans Res
+// Res <- (-0.5 + n/WIDTH)*scale + C
+__device__ void loadStart(int n, bool posC, uint32_t* decC, uint32_t* scale, bool* posRes, uint32_t* decRes)
+{
+	getStep(posRes, decRes);			// Res = 1/WIDTH
+	multIntIP(posRes, decRes, true, n);	// Res *= n
+	minusHalfIP(posRes, decRes);		// Res -= 0.5
+	multIP(posRes, decRes, true, scale);// Res *= scale
+	addIP(posRes, decRes, posC, decC);	// Res += C
+}
+
+// Boucle d'iteration principale.
+// Renvoie les couleurs calculees dans le tableau res.
 __device__ void computeMandel(uint32_t* res, bool* posXinit, uint32_t* decXinit, bool* posYinit, uint32_t* decYinit, bool* posX, uint32_t* decX, bool* posY, uint32_t* decY, bool* posTmp, uint32_t* decTmp)
 {
 	const unsigned int k = blockDim.z * blockIdx.z + threadIdx.z;
@@ -577,26 +465,7 @@ __device__ void computeMandel(uint32_t* res, bool* posXinit, uint32_t* decXinit,
 	__syncthreads();
 }
 
-// Charge le point de depart des iterations dans Res
-// Res = (-0.5 + i/WIDTH)*scale + C
-// C est le centre de la zone
-// x = i ou j
-// TODO : voir si passer k en parametre dans toutes les fonctions optimise.
-__device__ void loadStart(int n, bool posC, uint32_t* decC, uint32_t* scale, bool* posRes, uint32_t* decRes)
-{
-//#if WIDTH == 256
-//	getStep256(posRes, decRes);		// res = 1/256
-//#elif WIDTH == 512
-//	getStep512(posRes, decRes);		// res = 1/512
-//#endif
-	getStep(posRes, decRes);
-	multIntIP(posRes, decRes, true, n);	// Res *= i
-	minusHalfIP(posRes, decRes);		// Res -= 0.5
-	multIP(posRes, decRes, true, scale);// Res *= scale
-	addIP(posRes, decRes, posC, decC);	// Res += C
-}
-
-// Fonction principale
+// Fonction principale d'allocation de memoire
 __global__ void mainKernel(uint32_t* res, bool* posCx, uint32_t* decCx, bool* posCy, uint32_t* decCy, uint32_t* decS)
 {
 	const unsigned int ti = threadIdx.x;
@@ -625,69 +494,6 @@ __global__ void mainKernel(uint32_t* res, bool* posCx, uint32_t* decCx, bool* po
 		posXinit + ti, decXinit + ti*BIG_FLOAT_SIZE, posYinit + ti, decYinit + ti*BIG_FLOAT_SIZE, 
 		posX + ti, decX + ti*BIG_FLOAT_SIZE, posY + ti, decY + ti*BIG_FLOAT_SIZE, 
 		posTmp + ti, decTmp + ti*BIG_FLOAT_SIZE);
-}
-
-// Affiche la fractale de Mandelbrot centree en 0
-int computeBigMandelGPU(Affichage* display)
-{
-	uint32_t* d_res;
-	uint32_t* d_decCx;
-	uint32_t* d_decCy;
-	uint32_t* d_decS;
-	bool* d_posCx;
-	bool* d_posCy;
-	bool* d_posS;
-
-	ASSERT(cudaSuccess == cudaMalloc(&d_res, WIDTH * HEIGHT * sizeof(uint32_t)), "Device allocation of res failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_decCx, BIG_FLOAT_SIZE * sizeof(uint32_t)), "Device allocation of decCx failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_decCy, BIG_FLOAT_SIZE * sizeof(uint32_t)), "Device allocation of decCy failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_decS, BIG_FLOAT_SIZE * sizeof(uint32_t)), "Device allocation of decS failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_posCx, sizeof(bool)), "Device allocation of posCx failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_posCy, sizeof(bool)), "Device allocation of posCy failed", -1);
-	ASSERT(cudaSuccess == cudaMalloc(&d_posS, sizeof(bool)), "Device allocation of posS failed", -1);
-
-	uint32_t h_decCx[BIG_FLOAT_SIZE];
-	uint32_t h_decCy[BIG_FLOAT_SIZE];
-	uint32_t h_decS[BIG_FLOAT_SIZE];
-	bool h_posCx;
-	bool h_posCy;
-	bool h_posS;
-
-	for (int i = 0; i < BIG_FLOAT_SIZE; i++) {
-		h_decCx[i] = 0;
-		h_decCy[i] = 0;
-		h_decS[i] = 0;
-	}
-	h_decS[0] = 4;
-	h_posCx = true;
-	h_posCy = true;
-	h_posS = true;
-
-	ASSERT(cudaSuccess == cudaMemcpy(d_decCx, h_decCx, BIG_FLOAT_SIZE * sizeof(uint32_t), cudaMemcpyHostToDevice), "Copy of decCx from host to device failed", -1);
-	ASSERT(cudaSuccess == cudaMemcpy(d_posCx, &h_posCx, sizeof(bool), cudaMemcpyHostToDevice), "Copy of posCx from host to device failed", -1);
-	ASSERT(cudaSuccess == cudaMemcpy(d_decCy, h_decCy, BIG_FLOAT_SIZE * sizeof(uint32_t), cudaMemcpyHostToDevice), "Copy of decCy from host to device failed", -1);
-	ASSERT(cudaSuccess == cudaMemcpy(d_posCy, &h_posCy, sizeof(bool), cudaMemcpyHostToDevice), "Copy of posCy from host to device failed", -1);
-	ASSERT(cudaSuccess == cudaMemcpy(d_decS, h_decS, BIG_FLOAT_SIZE * sizeof(uint32_t), cudaMemcpyHostToDevice), "Copy of decS from host to device failed", -1);
-	ASSERT(cudaSuccess == cudaMemcpy(d_posS, &h_posS, sizeof(bool), cudaMemcpyHostToDevice), "Copy of posS from host to device failed", -1);
-
-	dim3 cudaBlockSize(BLOCK_X, BLOCK_Y, BIG_FLOAT_SIZE); // ATTENTION, 1024 threads max par block
-	dim3 cudaGridSize(WIDTH / BLOCK_X, HEIGHT / BLOCK_Y, 1); // ATTENTION : changer lorsque la grille n'est pas parfaitement adaptée a la fenetre
-	mainKernel << <cudaGridSize, cudaBlockSize >> >(d_res, d_posCx, d_decCx, d_posCy, d_decCy, d_decS);
-
-	ASSERT(cudaSuccess == cudaGetLastError(), "Kernel launch failed", -1);
-	ASSERT(cudaSuccess == cudaDeviceSynchronize(), "Kernel synchronization failed", -1);
-
-	ASSERT(cudaSuccess == cudaMemcpy(display->pixels, d_res, WIDTH * HEIGHT * sizeof(uint32_t), cudaMemcpyDeviceToHost), "Copy of decC from device to host failed", -1);
-	
-	ASSERT(cudaSuccess == cudaFree(d_res), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_decCx), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_decCy), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_decS), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_posCx), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_posCy), "Device deallocation failed", -1);
-	ASSERT(cudaSuccess == cudaFree(d_posS), "Device deallocation failed", -1);
-
-	return EXIT_SUCCESS;
 }
 
 // Affiche la fractale de Mandelbrot avec les dimensions passees en parametre
