19a524f582ba2eee4a18b58703aafff07fb49789
Etienne Ferrier <etienne.ferrier@hotmail.fr>
2015-03-08 22:36:01 +0100

    Correction d'un petit bug de dezoom sur la methode GPU simple

--- Parametres.hpp
+++ Parametres.hpp
@@ -8,8 +8,8 @@ Il est normalement inclus dans tous les fichiers du projet
 #define WIDTH 256					// Largeur de la fenetre
 #define HEIGHT 256					// Hauteur de la fenetre
 #define ZOOM_FACTOR 0.9f			// Facteur de zoom (par clic)
-#define DEZOOM_FACTOR 0.95f			// Facteur de dezoom (typiquement identique au facteur de zoom)
-#define NB_ITERATIONS 100			// Nombre d’itérations maximal
+#define DEZOOM_FACTOR 0.9f			// Facteur de dezoom (typiquement identique au facteur de zoom)
+#define NB_ITERATIONS 300			// Nombre d’itérations maximal
 #define GPU 1						// 1 if using GPU, 0 if using CPU
 #define BIG_FLOAT_SIZE 4			// Le nombre de bits sur lesquels on code un grand flottant, 0 pour utiliser les floats normaux
 #define INTERACTIVE 1				// Booléen décidant si le zoom est fixé ou interactif
